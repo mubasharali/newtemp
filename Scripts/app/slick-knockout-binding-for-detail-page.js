009e96809451d64da1bc9b1466fca1df96666317
mubasharali <irfanyusanif@gmail.com>
2016-04-22 21:14:17 +0500

    search tag api changed from post to get

--- Scripts/app/slick-knockout-binding-for-detail-page.js
+++ Scripts/app/slick-knockout-binding-for-detail-page.js
@@ -10,8 +10,8 @@
         $image.attr('src', imgUrl);
         // $div.append($image);
         $a.append($image);
-        // $div.append($a);
-        $div.append($image);
+         $div.append($a);
+      //  $div.append($image);
         return $div;
     };
 
